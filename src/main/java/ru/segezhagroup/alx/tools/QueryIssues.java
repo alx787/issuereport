1d7059af7e03f4cbd1e232d57185274e33d55dcf
kondratiev_aa <kaanova@gmail.com>
2021-10-28 18:43:37 +0300

    ui in setup

---
+++ src/main/java/ru/segezhagroup/alx/tools/QueryIssues.java
@@ -0,0 +1,13 @@
+package ru.segezhagroup.alx.tools;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.search.SearchProvider;
+import com.atlassian.jira.jql.parser.JqlQueryParser;
+import com.atlassian.jira.web.bean.PagerFilter;
+
+public class QueryIssues {
+
+//    SearchProvider searchProvider = ComponentAccessor.getComponent(SearchProvider.class);
+
+
+}
